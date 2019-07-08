151ef30cd0bb573b043d15dd99f67eabf1d23991
Ajeet Gathala <ajeetgathala@gmail.com>
2019-07-08 13:53:42 +0530

    Second

--- app/src/main/java/com/example/imagechooser/MainActivity.java
+++ app/src/main/java/com/example/imagechooser/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.imagechooser;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
@@ -9,6 +11,7 @@ import android.media.ExifInterface;
 import android.net.Uri;
 import android.os.Build;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
@@ -19,6 +22,8 @@ import android.widget.ImageView;
 import android.widget.Switch;
 
 import com.squareup.picasso.Picasso;
+import com.theartofdev.edmodo.cropper.CropImage;
+import com.theartofdev.edmodo.cropper.CropImageView;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
@@ -27,11 +32,12 @@ import java.io.InputStream;
 public class MainActivity extends AppCompatActivity {
 ImageView imageView;
 Button button;
-ImageView rotateLeft,rotateRight;
+ImageView rotateLeft,rotateRight,cropImg;
     Matrix matrix=new Matrix();
     Bitmap bitmap;
     Bitmap img;
     int angle=0;
+    Uri pickedImage;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,6 +46,13 @@ ImageView rotateLeft,rotateRight;
         button=findViewById(R.id.button);
         rotateLeft=findViewById(R.id.rotate_left);
         rotateRight=findViewById(R.id.rotate_right);
+        cropImg=findViewById(R.id.crop);
+        cropImg.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cropReq(pickedImage);
+            }
+        });
 
         rotateRight.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -84,8 +97,8 @@ ImageView rotateLeft,rotateRight;
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode==1&&resultCode==RESULT_OK&&data!=null){
-            Uri pickedImage=data.getData();
-
+            pickedImage=data.getData();
+            CropImage.ActivityResult result = CropImage.getActivityResult(data);
             InputStream inputStream;
 
             try {
@@ -107,6 +120,7 @@ ImageView rotateLeft,rotateRight;
                 imageView.setImageBitmap(bitmap);
                 rotateLeft.setVisibility(View.VISIBLE);
                 rotateRight.setVisibility(View.VISIBLE);
+                cropImg.setVisibility(View.VISIBLE);
 
             } catch (FileNotFoundException e) {
                 e.printStackTrace();
@@ -114,5 +128,46 @@ ImageView rotateLeft,rotateRight;
                 e.printStackTrace();
             }
         }
+        if (requestCode==CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE){
+            CropImage.ActivityResult result=CropImage.getActivityResult(data);
+            if (resultCode==RESULT_OK){
+                try {
+                    bitmap= MediaStore.Images.Media.getBitmap(getContentResolver(),result.getUri());
+                    imageView.setImageBitmap(bitmap);
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+    public void checkPermission(){
+        if (Build.VERSION.SDK_INT==Build.VERSION_CODES.M){
+            try {
+                requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE,Manifest.permission.READ_EXTERNAL_STORAGE},555);
+            }catch (Exception e){
+
+            }
+        }
+        else {
+            pickImg();
+        }
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        if (requestCode==555&&grantResults[0]== PackageManager.PERMISSION_GRANTED){
+
+            pickImg();
+        }else {
+            checkPermission();
+        }
+    }
+    public void pickImg(){
+        CropImage.startPickImageActivity(this);
+    }
+    public void cropReq(Uri uri){
+        CropImage.activity().setGuidelines(CropImageView.Guidelines.ON).setMultiTouchEnabled(true).start(this);
     }
 }
